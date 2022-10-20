56c7de6e9e6160089c027863aa047f172ace57e3
Anthony Bonilla <abonilla60953@ufide.ac.cr>
2022-10-20 17:56:32 -0600

    Version clase Semana#5

--- app/src/main/java/com/example/lugares_j/adapter/LugarAdapter.kt
+++ app/src/main/java/com/example/lugares_j/adapter/LugarAdapter.kt
@@ -2,9 +2,11 @@ package com.example.lugares_j.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lugares_j.databinding.LugarFilaBinding
 import com.example.lugares_j.model.Lugar
+import com.example.lugares_j.ui.lugar.LugarFragmentDirections
 
 class LugarAdapter : RecyclerView.Adapter<LugarAdapter.LugarViewHolder>(){
     inner class LugarViewHolder(private val itemBinding: LugarFilaBinding)
@@ -13,6 +15,13 @@ class LugarAdapter : RecyclerView.Adapter<LugarAdapter.LugarViewHolder>(){
             itemBinding.tvNombre.text = lugar.nombre
             itemBinding.tvCorreo.text = lugar.correo
             itemBinding.tvTelefono.text = lugar.telefono
+            itemBinding.vistaFila.setOnClickListener{
+                //creo una action para navegar al updqate lugar pasando un argumento lugar
+                val action = LugarFragmentDirections.actionNavLugarToUpdateLugarFragment(lugar)
+
+                //efectivamente se para el fragmento....
+                itemView.findNavController().navigate(action)
+            }
 
         }
 
